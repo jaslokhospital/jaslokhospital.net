b548a51c860f347989f7dce7695d0b58a935ea16
vishalchimote <vishal.chimote@asentec.com>
2016-08-31 22:04:28 +0530

    PopUp for subscription
    PopUp for subscription

--- public_html/JSControls/Common/JaslokTimesSubscription.ascx.cs
+++ public_html/JSControls/Common/JaslokTimesSubscription.ascx.cs
@@ -33,7 +33,14 @@ public partial class JSControls_Common_JaslokTimesSubscription : System.Web.UI.U
         objMailer.SendEmail("jasloktimes", lstParameters, EmailToId, EmailCCId);
         objMailer.SendEmail("jasloktimes_user", lstParameters, objDAEntities.Email, EmailCCId);
 
-        ScriptManager.RegisterStartupScript(this, GetType(), "showalert", "alert('You have subscribe Successfully!!!');", true);
+        string message = "You have subscribe Successfully!";
+        //dialog.Visible = true;
+        Page.ClientScript.RegisterStartupScript(this.GetType(), "Popup", "ShowPopup('" + message + "');", true);
+        // ScriptManager.RegisterStartupScript(this, GetType(), "showalert", "alert('You have subscribe Successfully!!!');", true);
         txtemail.Text = "";
+
+
+
+        //ScriptManager.RegisterStartupScript(this, GetType(), "showalert", "alert('You have subscribe Successfully!!!');", true);
     }
 }
